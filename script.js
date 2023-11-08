e7cf83dec7b97a9a6f4c80c6fe7a2adf0f45ba7a
Johnny Trinh <johnny.trinh@hotmail.se>
2023-11-08 17:08:02 +0100

    Adding navbar toggle

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function runTypingEffect() {
-  const text = 'I am Johnny "Domestico" Trinh.';
-  const typingElement = document.getElementById('typing-text');
-  const typingDelay = 100;
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
-  typeText(text, typingElement, typingDelay);
-}
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+  const linksHeight = links.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});

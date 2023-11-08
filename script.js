921e159a3b0b2d2f07712af9154c6c28be9d48b0
Johnny Trinh <johnny.trinh@hotmail.se>
2023-11-08 13:38:05 +0100

    Setup json, bootstrap, script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function runTypingEffect() {
+  const text = 'I am Johnny "Domestico" Trinh.';
+  const typingElement = document.getElementById('typing-text');
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
